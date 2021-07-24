9fded9684d9eb043d5d353a7b905b7ed6b7acc42
Guilherme Ribeiro <augusto.gui.2208@gmail.com>
2021-07-23 23:37:13 -0300

    mudando class para ClassName

--- src/components/TweetPost/TweetPost.jsx
+++ src/components/TweetPost/TweetPost.jsx
@@ -25,7 +25,7 @@ const TweetPost = ({ children }) => {
                      <svg
                         viewBox="0 0 24 24"
                         aria-hidden="true"
-                        class="r-4qtqp9 r-yyyyoo r-1xvli5t r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-1hdv0qi"
+                        className="r-4qtqp9 r-yyyyoo r-1xvli5t r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-1hdv0qi"
                      >
                         <g>
                            <circle cx="5" cy="12" r="2"></circle>
