f912166b610211ab4ff3d2a99d8550aa0289a527
Guilherme Ribeiro <augusto.gui.2208@gmail.com>
2021-08-09 12:54:08 -0300

    build

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
    return (
       <Router>
          <Switch>
-           <Route path="/home" exact component={Home}/>
+           <Route path="/" exact component={Home}/>
            <Route path="/compose/tweet" component={TweetMobile}/>
          </Switch>
       </Router>
